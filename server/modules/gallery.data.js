b8808eba7850c7502a60535c0e291ba182ee2da7
Weizhen Wang <wangwzhen@gmail.com>
2019-06-07 22:27:49 -0500

    data file on server modified with new photos data added

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,13 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/Italy.jpg', description: 'Photo of Cinque Terre in Italy.', likes: 0 },
+    { id: 3, path: 'images/Finland.jpg', description: 'Photo of a building in Helsinki, Finland.', likes: 0 },
+    { id: 4, path: 'images/Hungary.jpg', description: 'Photo of the castle view across the river.', likes: 0 },
+    { id: 5, path: 'images/Iceland.jpg', description: 'Photo of the glaciers floating on the lagoon.', likes: 0 },
+    { id: 6, path: 'images/Russia.jpg', description: 'Photo of Church of the Savior on Blood in Saint Petersburg.', likes: 0 },
+    { id: 7, path: 'images/Singapore.jpg', description: 'Photo of the city view.', likes: 0 },
+    { id: 8, path: 'images/Washington_DC.jpg', description: 'Photo of China Town in DC.', likes: 0 },
+    { id: 9, path: 'images/Yellow_stone.jpg', description: 'Photo of the geyser in a mist morning.', likes: 0 },
 ];
 
 module.exports = galleryItems;
